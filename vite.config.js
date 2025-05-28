a7d5cf3c20b0e231bae2d6f147f8d3dedf9d5ead
mggmz <tonykill3@gmail.com>
2025-05-28 11:40:18 -0600

    fix: corregir la ruta del script en index.html y ajustar la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/DevPattern/",   // carpeta que creará GitHub Pages
+  base: mode === "production" ? "/DevPattern/" : "/", // Solo usar subcarpeta en producción
   server: {
-    https: false// para desarrollar localmente con HTTPS (autofirmado)
+    https: false // para desarrollar localmente con HTTPS (autofirmado)
   }
-});
+}));
